efe5d928c46b430ab691eb104c90560370b42894
Michael Fitzmayer <mail@michael-fitzmayer.de>
2022-01-22 23:25:10 +0100

    Minor cleanup

--- modules/SDLmain.cmake
+++ modules/SDLmain.cmake
@@ -4,8 +4,8 @@ set(GCC_MODULE_COMN_DEFS -D__SYMBIAN32__ -D__GCC32__ -D__EPOC32__ -D__MARM__ -D_
 set(GCC_MODULE_MODE_DEFS -DNDEBUG -D_UNICODE)
 set(GCC_MODULE_DEFS      ${GCC_MODULE_COMN_DEFS} ${GCC_MODULE_MODE_DEFS})
 
-set(SRC_DIR       "${CMAKE_CURRENT_SOURCE_DIR}/../../projects/SDL-1.2/src")
-set(SDL_DIR       "${SRC_DIR}/SDL")
+set(SRC_DIR "${CMAKE_CURRENT_SOURCE_DIR}/../../projects/SDL-1.2/src")
+set(SDL_DIR "${SRC_DIR}/SDL")
 
 set(SDLmain_sources
     "${SDL_DIR}/src/main/epoc/SDL_main.cpp")
