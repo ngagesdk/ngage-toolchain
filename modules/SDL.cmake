ca754252ec78cf56bc137b0628aa79f9c50fe826
Michael Fitzmayer <mail@michael-fitzmayer.de>
2022-01-22 23:24:16 +0100

    Update variable name to avoid possible conflicts

--- modules/SDL.cmake
+++ modules/SDL.cmake
@@ -1,8 +1,8 @@
 # SDL 1.2
 
-set(GCC_COMN_DEFS -D__SYMBIAN32__ -D__GCC32__ -D__EPOC32__ -D__MARM__ -D__MARM_ARMI__)
-set(GCC_MODE_DEFS -DNDEBUG -D_UNICODE)
-set(GCC_DEFS      ${GCC_COMN_DEFS} ${GCC_MODE_DEFS})
+set(GCC_MODULE_COMN_DEFS -D__SYMBIAN32__ -D__GCC32__ -D__EPOC32__ -D__MARM__ -D__MARM_ARMI__)
+set(GCC_MODULE_MODE_DEFS -DNDEBUG -D_UNICODE)
+set(GCC_MODULE_DEFS      ${GCC_MODULE_COMN_DEFS} ${GCC_MODULE_MODE_DEFS})
 
 set(SRC_DIR       "${CMAKE_CURRENT_SOURCE_DIR}/../../projects/SDL-1.2/src")
 set(SDL_DIR       "${SRC_DIR}/SDL")
@@ -72,7 +72,7 @@ target_compile_definitions(
     ENABLE_EPOC
     DISABLE_JOYSTICK
     DISABLE_CDROM
-    ${GCC_DEFS})
+    ${GCC_MODULE_DEFS})
 
 target_include_directories(
     SDL
--- modules/SDLmain.cmake
+++ modules/SDLmain.cmake
@@ -1,8 +1,8 @@
 # SDLmain
 
-set(GCC_COMN_DEFS -D__SYMBIAN32__ -D__GCC32__ -D__EPOC32__ -D__MARM__ -D__MARM_ARMI__)
-set(GCC_MODE_DEFS -DNDEBUG -D_UNICODE)
-set(GCC_DEFS      ${GCC_COMN_DEFS} ${GCC_MODE_DEFS})
+set(GCC_MODULE_COMN_DEFS -D__SYMBIAN32__ -D__GCC32__ -D__EPOC32__ -D__MARM__ -D__MARM_ARMI__)
+set(GCC_MODULE_MODE_DEFS -DNDEBUG -D_UNICODE)
+set(GCC_MODULE_DEFS      ${GCC_MODULE_COMN_DEFS} ${GCC_MODULE_MODE_DEFS})
 
 set(SRC_DIR       "${CMAKE_CURRENT_SOURCE_DIR}/../../projects/SDL-1.2/src")
 set(SDL_DIR       "${SRC_DIR}/SDL")
@@ -15,7 +15,7 @@ add_library(SDLmain STATIC ${SDLmain_sources})
 target_compile_definitions(
     SDLmain
     PUBLIC
-    ${GCC_DEFS})
+    ${GCC_MODULE_DEFS})
 
 target_include_directories(
     SDLmain
