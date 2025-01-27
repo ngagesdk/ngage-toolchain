f9e5b388bab40e078e48a6b11037fc082e986961
Michael Fitzmayer <mail@michael-fitzmayer.de>
2025-01-27 19:08:36 +0100

    Rename demo project to celeste, update files accordingly

--- projects/celeste/cmake/project_config.cmake
+++ projects/celeste/cmake/project_config.cmake
@@ -1,6 +1,6 @@
 # Project configuration.
 
-set(PROJECT_TITLE "demo")
+set(PROJECT_TITLE "celeste")
 
 set(project_sources
     ${CMAKE_CURRENT_SOURCE_DIR}/src/main.c
@@ -9,4 +9,4 @@ set(project_sources
 
 set(UID3 0x10005799) # game.exe UID
 set(APP_UID 0x10005800) # launcher.app UID
-set(APP_NAME "demo")
+set(APP_NAME "celeste")
