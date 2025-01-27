8bf5095c667e14253282055246d167d504123fef
Michael Fitzmayer <mail@michael-fitzmayer.de>
2025-01-27 20:09:15 +0100

    Update celeste main application, yet unfinished

--- projects/celeste/src/main.c
+++ projects/celeste/src/main.c
@@ -12,6 +12,7 @@
 
 #include <assert.h>
 #include <stdio.h>
+#include <time.h>
 
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
@@ -61,14 +62,14 @@ static void SetPaletteEntry(unsigned char idx, unsigned char base_idx)
 {
     palette[idx] = base_palette[base_idx];
 
-    SDL_PixelFormatDetails* details = SDL_GetPixelFormatDetails(screen->format);
+    const SDL_PixelFormatDetails* details = SDL_GetPixelFormatDetails(screen->format);
     map[idx] = SDL_MapRGB(details, NULL, palette[idx].r, palette[idx].g, palette[idx].b);
 }
 
 static void RefreshPalette(void)
 {
     int i;
-    SDL_PixelFormatDetails* details = SDL_GetPixelFormatDetails(screen->format);
+    const SDL_PixelFormatDetails* details = SDL_GetPixelFormatDetails(screen->format);
 
     for (i = 0; i < SDL_arraysize(map); i++)
     {
@@ -222,8 +223,8 @@ static void OSDdraw(void)
 
 static void Flip(SDL_Surface* screen)
 {
-    SDL_Rect source = { 0, 0, 128, 128 };
-    SDL_Rect dest   = { 24, 40, 128, 128 };
+    SDL_FRect source = { 0, 0, 128, 128 };
+    SDL_FRect dest   = { 24, 40, 128, 128 };
 
     SDL_UpdateTexture(SDL_screen_tex, NULL, screen->pixels, screen->pitch);
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
@@ -236,7 +237,6 @@ static bool paused = false;
 static bool running = true;
 static void* initial_game_state = NULL;
 static void* game_state = NULL;
-static void  mainLoop(void);
 static FILE* TAS = NULL;
 
 // This function runs once at startup.
@@ -366,7 +366,43 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 // This function runs once per frame, and is the heart of the program.
 SDL_AppResult SDL_AppIterate(void *appstate)
 {
-    SDL_RenderPresent(renderer);
+    static int reset_input_timer = 0;
+
+    if (initial_game_state != NULL)
+    {
+        reset_input_timer++;
+        if (reset_input_timer >= 30)
+        {
+            reset_input_timer=0;
+            //reset
+            OSDset("reset");
+            paused = 0;
+            Celeste_P8_load_state(initial_game_state);
+            Celeste_P8_set_rndseed((unsigned)(time(NULL) + SDL_GetTicks()));
+            Celeste_P8_init();
+        }
+    }
+    else
+    {
+        reset_input_timer = 0;
+    }
+
+    if (paused)
+    {
+        const int x0 = PICO8_W/2-3*4, y0 = 8;
+
+        p8_rectfill(x0-1,y0-1, 6*4+x0+1,6+y0+1, 6);
+        p8_rectfill(x0,y0, 6*4+x0,6+y0, 0);
+        p8_print("paused", x0+1, y0+1, 7);
+    }
+    else
+    {
+        Celeste_P8_update();
+        Celeste_P8_draw();
+    }
+    OSDdraw();
+    Flip(screen);
+
     return SDL_APP_CONTINUE;
 }
 
@@ -379,4 +415,537 @@ void SDL_AppQuit(void *appstate, SDL_AppResult result)
     }
 
     // SDL will clean up the window/renderer for us.
+}
+
+static int gettileflag(int tile, int flag);
+static void p8_line(int x0, int y0, int x1, int y1, unsigned char color);
+
+//lots of code from https://github.com/SDL-mirror/SDL/blob/bc59d0d4a2c814900a506d097a381077b9310509/src/video/SDL_surface.c#L625
+//coordinates should be scaled already
+static inline void Xblit(SDL_Surface* src, SDL_Rect* srcrect, SDL_Surface* dst, SDL_Rect* dstrect, int color, int flipx, int flipy)
+{
+    SDL_Rect fulldst;
+    int srcx, srcy, w, h;
+
+    assert(src && dst && !src->locked && !dst->locked);
+    //assert(src->format->BitsPerPixel == 8);
+    //assert(dst->format->BytesPerPixel == 2 || dst->format->BytesPerPixel == 4);
+    /* If the destination rectangle is NULL, use the entire dest surface */
+    if (!dstrect)
+    {
+        dstrect = (fulldst = (SDL_Rect){ 0,0,dst->w,dst->h }, &fulldst);
+    }
+
+    /* clip the source rectangle to the source surface */
+    if (srcrect)
+    {
+        int maxw, maxh;
+
+        srcx = srcrect->x;
+        w = srcrect->w;
+        if (srcx < 0)
+        {
+            w += srcx;
+            dstrect->x -= srcx;
+            srcx = 0;
+        }
+        maxw = src->w - srcx;
+        if (maxw < w)
+        {
+            w = maxw;
+        }
+
+        srcy = srcrect->y;
+        h = srcrect->h;
+        if (srcy < 0)
+        {
+            h += srcy;
+            dstrect->y -= srcy;
+            srcy = 0;
+        }
+        maxh = src->h - srcy;
+        if (maxh < h)
+        {
+            h = maxh;
+        }
+
+    }
+    else
+    {
+        srcx = srcy = 0;
+        w = src->w;
+        h = src->h;
+    }
+
+    /* clip the destination rectangle against the clip rectangle */
+    {
+        SDL_Rect clip;
+        clip.x = 0;
+        clip.y = 0;
+        clip.w = dst->w;
+        clip.h = dst->h;
+
+        int dx, dy;
+
+        dx = clip.x - dstrect->x;
+        if (dx > 0)
+        {
+            w -= dx;
+            dstrect->x += dx;
+            srcx += dx;
+        }
+        dx = dstrect->x + w - clip.x - clip.w;
+        if (dx > 0)
+        {
+            w -= dx;
+        }
+
+        dy = clip.y - dstrect->y;
+        if (dy > 0)
+        {
+            h -= dy;
+            dstrect->y += dy;
+            srcy += dy;
+        }
+        dy = dstrect->y + h - clip.y - clip.h;
+        if (dy > 0)
+        {
+            h -= dy;
+        }
+    }
+
+    if (w && h)
+    {
+        unsigned char* srcpix = src->pixels;
+        int srcpitch = src->pitch;
+        int x, y;
+
+#define _blitter(dsttype, dp, xflip) do { \
+            dsttype* dstpix = dst->pixels; \
+            for (y = 0; y < h; y++) \
+            { \
+                for (x = 0; x < w; x++) \
+                { \
+                    unsigned char p = srcpix[!xflip ? srcx+x+(srcy+y)*srcpitch : srcx+(w-x-1)+(srcy+y)*srcpitch]; \
+                    if (p) dstpix[dstrect->x+x + (dstrect->y+y)*dst->w] = getcolor(dp); \
+                } \
+            } \
+            } while(0)
+
+        int screen_bpp = SDL_BYTESPERPIXEL(screen->format);
+        if (screen_bpp == 2 && color && flipx)
+        {
+            _blitter(Uint16, color, 1);
+        }
+        else if (screen_bpp == 2 && !color && flipx)
+        {
+            _blitter(Uint16, p, 1);
+        }
+        else if (screen_bpp == 2 && color && !flipx)
+        {
+            _blitter(Uint16, color, 0);
+        }
+        else if (screen_bpp == 2 && !color && !flipx)
+        {
+            _blitter(Uint16, p, 0);
+        }
+        else if (screen_bpp == 4 && color && flipx)
+        {
+            _blitter(Uint32, color, 1);
+        }
+        else if (screen_bpp == 4 && !color && flipx)
+        {
+            _blitter(Uint32, p, 1);
+        }
+        else if (screen_bpp == 4 && color && !flipx)
+        {
+            _blitter(Uint32, color, 0);
+        }
+        else if (screen_bpp == 4 && !color && !flipx)
+        {
+            _blitter(Uint32, p, 0);
+        }
+#undef _blitter
+    }
+}
+
+static void p8_rectfill(int x0, int y0, int x1, int y1, int col)
+{
+    int w = (x1 - x0 + 1) * scale;
+    int h = (y1 - y0 + 1) * scale;
+    if (w > 0 && h > 0)
+    {
+        SDL_Rect rc = { x0 * scale,y0 * scale, w,h };
+        SDL_FillSurfaceRect(screen, &rc, getcolor(col));
+    }
+}
+
+static void p8_print(const char* str, int x, int y, int col)
+{
+    char c;
+    for (c = *str; c; c = *(++str))
+    {
+        SDL_Rect srcrc;
+        SDL_Rect dstrc;
+        c &= 0x7F;
+        srcrc.x = 8 * (c % 16);
+        srcrc.y = 8 * (c / 16);
+        srcrc.x *= scale;
+        srcrc.y *= scale;
+        srcrc.w = srcrc.h = 8 * scale;
+
+        dstrc.x = x * scale;
+        dstrc.y = y * scale;
+        dstrc.w = scale;
+        dstrc.h = scale;
+
+        Xblit(font, &srcrc, screen, &dstrc, col, 0, 0);
+        x += 4;
+    }
+}
+
+int pico8emu(CELESTE_P8_CALLBACK_TYPE call, ...)
+{
+    static int camera_x = 0, camera_y = 0;
+    va_list    args;
+    int        ret = 0;
+
+    if (!enable_screenshake)
+    {
+        camera_x = camera_y = 0;
+    }
+
+    va_start(args, call);
+
+#define INT_ARG() va_arg(args, int)
+#define BOOL_ARG() (bool)va_arg(args, int)
+#define RET_INT(_i) do {ret = (_i); goto end;} while (0)
+#define RET_BOOL(_b) RET_INT(!!(_b))
+
+    switch (call)
+    {
+        case CELESTE_P8_SPR: //spr(sprite,x,y,cols,rows,flipx,flipy)
+        {
+            int sprite = INT_ARG();
+            int x = INT_ARG();
+            int y = INT_ARG();
+            int cols = INT_ARG();
+            int rows = INT_ARG();
+            int flipx = BOOL_ARG();
+            int flipy = BOOL_ARG();
+
+            (void)cols;
+            (void)rows;
+
+            assert(rows == 1 && cols == 1);
+
+            if (sprite >= 0)
+            {
+                SDL_Rect srcrc = {
+                    8 * (sprite % 16),
+                    8 * (sprite / 16)
+                };
+                SDL_Rect dstrc =
+                {
+                (x - camera_x) * scale, (y - camera_y) * scale,
+                scale, scale
+                };
+                srcrc.x *= scale;
+                srcrc.y *= scale;
+                srcrc.w = srcrc.h = scale * 8;
+                Xblit(gfx, &srcrc, screen, &dstrc, 0, flipx, flipy);
+            }
+            break;
+        }
+        case CELESTE_P8_BTN: //btn(b)
+        {
+            int b = INT_ARG();
+            assert(b >= 0 && b <= 5);
+            RET_BOOL(buttons_state & (1 << b));
+            break;
+        }
+        case CELESTE_P8_PAL: //pal(a,b)
+        {
+            int a = INT_ARG();
+            int b = INT_ARG();
+            if (a >= 0 && a < 16 && b >= 0 && b < 16)
+            {
+                //swap palette colors
+                SetPaletteEntry(a, b);
+            }
+            break;
+        }
+        case CELESTE_P8_PAL_RESET: //pal()
+        {
+            ResetPalette();
+            break;
+        }
+        case CELESTE_P8_CIRCFILL: //circfill(x,y,r,col)
+        {
+            int cx = INT_ARG() - camera_x;
+            int cy = INT_ARG() - camera_y;
+            int r = INT_ARG();
+            int col = INT_ARG();
+            int realcolor = getcolor(col);
+
+            if (r <= 1)
+            {
+                SDL_Rect rect_a = { scale * (cx - 1), scale * cy, scale * 3, scale };
+                SDL_Rect rect_b = { scale * cx, scale * (cy - 1), scale, scale * 3 };
+
+                SDL_FillSurfaceRect(screen, &rect_a, realcolor);
+                SDL_FillSurfaceRect(screen, &rect_b, realcolor);
+            }
+            else if (r <= 2)
+            {
+                SDL_Rect rect_a = { scale * (cx - 2), scale * (cy - 1), scale * 5, scale * 3 };
+                SDL_Rect rect_b = { scale * (cx - 1), scale * (cy - 2), scale * 3, scale * 5 };
+
+                SDL_FillSurfaceRect(screen, &rect_a, realcolor);
+                SDL_FillSurfaceRect(screen, &rect_b, realcolor);
+            }
+            else if (r <= 3)
+            {
+                SDL_Rect rect_a = { scale * (cx - 3), scale * (cy - 1), scale * 7, scale * 3 };
+                SDL_Rect rect_b = { scale * (cx - 1), scale * (cy - 3), scale * 3, scale * 7 };
+                SDL_Rect rect_c = { scale * (cx - 2), scale * (cy - 2), scale * 5, scale * 5 };
+
+                SDL_FillSurfaceRect(screen, &rect_a, realcolor);
+                SDL_FillSurfaceRect(screen, &rect_b, realcolor);
+                SDL_FillSurfaceRect(screen, &rect_c, realcolor);
+            }
+            else  //i dont think the game uses this
+            {
+                int f = 1 - r; //used to track the progress of the drawn circle (since its semi-recursive)
+                int ddFx = 1;   //step x
+                int ddFy = -2 * r; //step y
+                int x = 0;
+                int y = r;
+
+                //this algorithm doesn't account for the diameters
+                //so we have to set them manually
+                p8_line(cx, cy - y, cx, cy + r, col);
+                p8_line(cx + r, cy, cx - r, cy, col);
+
+                while (x < y)
+                {
+                    if (f >= 0)
+                    {
+                        y--;
+                        ddFy += 2;
+                        f += ddFy;
+                    }
+                    x++;
+                    ddFx += 2;
+                    f += ddFx;
+
+                    //build our current arc
+                    p8_line(cx + x, cy + y, cx - x, cy + y, col);
+                    p8_line(cx + x, cy - y, cx - x, cy - y, col);
+                    p8_line(cx + y, cy + x, cx - y, cy + x, col);
+                    p8_line(cx + y, cy - x, cx - y, cy - x, col);
+                }
+            }
+            break;
+        }
+        case CELESTE_P8_PRINT: //print(str,x,y,col)
+        {
+            const char* str = va_arg(args, const char*);
+            int x = INT_ARG() - camera_x;
+            int y = INT_ARG() - camera_y;
+            int col = INT_ARG() % 16;
+
+            p8_print(str, x, y, col);
+            break;
+        }
+        case CELESTE_P8_RECTFILL: //rectfill(x0,y0,x1,y1,col)
+        {
+            int x0 = INT_ARG() - camera_x;
+            int y0 = INT_ARG() - camera_y;
+            int x1 = INT_ARG() - camera_x;
+            int y1 = INT_ARG() - camera_y;
+            int col = INT_ARG();
+
+            p8_rectfill(x0, y0, x1, y1, col);
+            break;
+        }
+        case CELESTE_P8_LINE: //line(x0,y0,x1,y1,col)
+        {
+            int x0 = INT_ARG() - camera_x;
+            int y0 = INT_ARG() - camera_y;
+            int x1 = INT_ARG() - camera_x;
+            int y1 = INT_ARG() - camera_y;
+            int col = INT_ARG();
+
+            p8_line(x0, y0, x1, y1, col);
+            break;
+        }
+        case CELESTE_P8_MGET: //mget(tx,ty)
+        {
+            int tx = INT_ARG();
+            int ty = INT_ARG();
+
+            RET_INT(tilemap_data[tx + ty * 128]);
+            break;
+        }
+        case CELESTE_P8_CAMERA: //camera(x,y)
+        {
+            if (enable_screenshake)
+            {
+                camera_x = INT_ARG();
+                camera_y = INT_ARG();
+            }
+            break;
+        }
+        case CELESTE_P8_FGET: //fget(tile,flag)
+        {
+            int tile = INT_ARG();
+            int flag = INT_ARG();
+
+            RET_INT(gettileflag(tile, flag));
+            break;
+        }
+        case CELESTE_P8_MAP: //map(mx,my,tx,ty,mw,mh,mask)
+        {
+            int mx = INT_ARG(), my = INT_ARG();
+            int tx = INT_ARG(), ty = INT_ARG();
+            int mw = INT_ARG(), mh = INT_ARG();
+            int mask = INT_ARG();
+            int x, y;
+
+            for (x = 0; x < mw; x++)
+            {
+                for (y = 0; y < mh; y++)
+                {
+                    int tile = tilemap_data[x + mx + (y + my) * 128];
+                    //hack
+                    if (mask == 0 || (mask == 4 && tile_flags[tile] == 4) || gettileflag(tile, mask != 4 ? mask - 1 : mask))
+                    {
+                        SDL_Rect srcrc =
+                        {
+                            8 * (tile % 16),
+                            8 * (tile / 16)
+                        };
+                        SDL_Rect dstrc =
+                        {
+                            (tx + x * 8 - camera_x) * scale, (ty + y * 8 - camera_y) * scale,
+                            scale * 8, scale * 8
+                        };
+                        srcrc.x *= scale;
+                        srcrc.y *= scale;
+                        srcrc.w = srcrc.h = scale * 8;
+
+                        /*
+                        if (0)
+                        {
+                            srcrc.x = srcrc.y = 0;
+                            srcrc.w = srcrc.h = 8;
+                            dstrc.x = x*8, dstrc.y = y*8;
+                            dstrc.w = dstrc.h = 8;
+                        }
+                        */
+
+                        Xblit(gfx, &srcrc, screen, &dstrc, 0, 0, 0);
+                    }
+                }
+            }
+            break;
+        }
+    }
+
+end:
+    va_end(args);
+    return ret;
+}
+
+static int gettileflag(int tile, int flag)
+{
+    return tile < sizeof(tile_flags) / sizeof(*tile_flags) && (tile_flags[tile] & (1 << flag)) != 0;
+}
+
+//coordinates should NOT be scaled before calling this
+static void p8_line(int x0, int y0, int x1, int y1, unsigned char color)
+{
+    Uint32   realcolor = getcolor(color);
+    int      sx, sy, dx, dy, err;
+    SDL_Rect rect;
+
+#define CLAMP(v,min,max) v = v < min ? min : v >= max ? max-1 : v;
+    CLAMP(x0, 0, screen->w);
+    CLAMP(y0, 0, screen->h);
+    CLAMP(x1, 0, screen->w);
+    CLAMP(y1, 0, screen->h);
+
+#undef CLAMP
+#define PLOT(xa, ya) \
+    rect.x = xa * scale; \
+    rect.y = ya * scale; \
+    rect.w = scale; \
+    rect.h = scale; \
+    do { \
+        SDL_FillSurfaceRect(screen, &rect, realcolor); \
+    } \
+    while (0)
+    dx = abs(x1 - x0);
+    dy = abs(y1 - y0);
+    if (!dx && !dy)
+    {
+        return;
+    }
+
+    if (x0 < x1)
+    {
+        sx = 1;
+    }
+    else
+    {
+        sx = -1;
+    }
+    if (y0 < y1)
+    {
+        sy = 1;
+    }
+    else
+    {
+        sy = -1;
+    }
+    err = dx - dy;
+    if (!dy && !dx)
+    {
+        return;
+    }
+    else if (!dx) //vertical line
+    {
+        int y;
+        for (y = y0; y != y1; y += sy)
+        {
+            PLOT(x0, y);
+        }
+    }
+    else if (!dy) //horizontal line
+    {
+        int x;
+        for (x = x0; x != x1; x += sx)
+        {
+            PLOT(x, y0);
+        }
+    }
+
+    while (x0 != x1 || y0 != y1)
+    {
+        int e2;
+        PLOT(x0, y0);
+        e2 = 2 * err;
+        if (e2 > -dy)
+        {
+            err -= dy;
+            x0 += sx;
+        }
+        if (e2 < dx)
+        {
+            err += dx;
+            y0 += sy;
+        }
+    }
+#undef PLOT
 }
