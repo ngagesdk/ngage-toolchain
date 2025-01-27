a19661701596536ceec16d1ca755ec945f447364
Michael Fitzmayer <mail@michael-fitzmayer.de>
2025-01-27 18:13:27 +0100

    Copy celeste source from previous N-Gage adaption (SDL2 version)

---
+++ projects/demo/src/celeste.h
@@ -0,0 +1,40 @@
+#ifndef CELESTE_H_
+#define CELESTE_H_
+
+#ifdef __cplusplus
+#define _Bool bool
+extern "C" {
+#else
+#define _Bool int
+#endif
+
+typedef enum
+{
+    CELESTE_P8_MUSIC, CELESTE_P8_SPR, CELESTE_P8_BTN, CELESTE_P8_SFX,
+    CELESTE_P8_PAL, CELESTE_P8_PAL_RESET, CELESTE_P8_CIRCFILL, CELESTE_P8_PRINT,
+    CELESTE_P8_RECTFILL, CELESTE_P8_LINE, CELESTE_P8_MGET, CELESTE_P8_CAMERA,
+    CELESTE_P8_FGET, CELESTE_P8_MAP
+
+} CELESTE_P8_CALLBACK_TYPE;
+
+typedef _Bool Celeste_P8_bool_t;
+typedef int (*Celeste_P8_cb_func_t) (CELESTE_P8_CALLBACK_TYPE calltype, ...);
+
+extern void Celeste_P8_set_call_func(Celeste_P8_cb_func_t func);
+extern void Celeste_P8_set_rndseed(unsigned seed);
+extern void Celeste_P8_init(void);
+extern void Celeste_P8_update(void);
+extern void Celeste_P8_draw(void);
+
+extern void Celeste_P8__DEBUG(void); //debug functionality
+
+//state functionality
+size_t Celeste_P8_get_state_size(void);
+void Celeste_P8_save_state(void* st);
+void Celeste_P8_load_state(const void* st);
+
+#ifdef __cplusplus
+} //extern "C"
+#endif
+
+#endif
