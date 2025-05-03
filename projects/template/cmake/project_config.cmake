262a027c461f8b1a98087ad47f29a11376273958
Michael Fitzmayer <mail@michael-fitzmayer.de>
2025-05-03 09:13:44 +0200

    Update template projects, remove linking against ngagesdk_entry.o

--- projects/template/cmake/project_config.cmake
+++
@@ -1,8 +0,0 @@
-# Project configuration.
-
-set(project_sources
-  ${CMAKE_CURRENT_SOURCE_DIR}/src/main.c)
-
-set(UID3 0x1000c37e) # game.exe UID
-set(APP_UID 0x10005777) # template.app UID
-set(APP_NAME "template")
